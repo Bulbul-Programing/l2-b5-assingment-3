5e2c7f98da82ce286e1f5bdacf76110ea31f30cb
Bulbul-Programinge <bulbul.programing@gmail.com>
2025-06-25 19:20:11 +0600

    quantity update section done

--- src/app/controller/borrow.controller.ts
+++ src/app/controller/borrow.controller.ts
@@ -14,17 +14,33 @@ BorrowRoute.post('/', async (req: Request, res: Response) => {
         if (!isExistBook) {
             res.status(400).json({
                 "success": false,
-                "message": 'Book Not Found!',
+                "message": 'Book Not Found'
             })
         }
 
-        const result = await BorrowModel.create(borrowData)
+        else if (isExistBook?.copies < borrowData.quantity) {
+            res.status(400).json({
+                "success": false,
+                "message": 'Book Quantity is must be less than book quantity'
+            })
+        }
+        else if (new Date().getTime() > new Date(borrowData.dueDate).getTime()) {
+            res.status(400).json({
+                "success": false,
+                "message": 'Please Select feature Date'
+            })
+        }
+        else {
+            const updateCopy = BorrowModel.updateBorrowCopy(borrowData.book, Number(borrowData.quantity), isExistBook)
+            const result = await BorrowModel.create(borrowData)
+            res.status(200).json({
+                "success": true,
+                "message": "Book borrowed successfully",
+                "data": result
+            })
+        }
 
-        res.status(200).json({
-            "success": true,
-            "message": "Book borrowed successfully",
-            "data": result
-        })
+
     }
     catch (error: any) {
         res.status(400).json({
--- src/app/models/borrow.model.ts
+++ src/app/models/borrow.model.ts
@@ -1,7 +1,13 @@
-import { model, Schema } from "mongoose";
+import { Model, model, Schema } from "mongoose";
 import { TBorrow } from "../interface/borrow .interface";
+import { Book } from "./book.model";
+import { TBook } from "../interface/book.interface";
 
-const borrowSchema = new Schema<TBorrow>({
+interface borrowInstanceMethod extends Model<TBorrow> {
+    updateBorrowCopy(bookId: string, bookCopy: number, borrowBookInfo: TBook): any
+}
+
+const borrowSchema = new Schema<TBorrow, borrowInstanceMethod>({
     book: { type: Schema.ObjectId, required: true },
     quantity: { type: Number, required: true },
     dueDate: { type: Date, required: true }
@@ -10,4 +16,12 @@ const borrowSchema = new Schema<TBorrow>({
     timestamps: true
 })
 
-export const BorrowModel = model<TBorrow>('brow', borrowSchema)
+borrowSchema.static('updateBorrowCopy', async function updateBorrowCopy(bookId: string, bookCopy: number, borrowBookInfo: TBook) {
+    const updateDoc = {
+        copies: borrowBookInfo.copies - bookCopy,
+        available: borrowBookInfo.copies - bookCopy === 0 ? false : true
+    }
+    await Book.findByIdAndUpdate(bookId, updateDoc)
+})
+
+export const BorrowModel = model<TBorrow, borrowInstanceMethod>('brow', borrowSchema)
